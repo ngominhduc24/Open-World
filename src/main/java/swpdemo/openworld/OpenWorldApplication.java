29ebb3023a54dff8523584ea39ca6e374fe802a1
Minh Duc <ngominhduc100@gmail.com>
2023-08-17 16:17:09 +0700

    init

---
+++ src/main/java/swpdemo/openworld/OpenWorldApplication.java
@@ -0,0 +1,13 @@
+package swpdemo.openworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swpdemo/openworld/OpenWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package swpdemo.openworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
