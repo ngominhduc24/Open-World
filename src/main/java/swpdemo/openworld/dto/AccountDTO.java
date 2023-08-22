a729b1ad5c2bbc89ff54749dd690771dc2d03f63
Minh Duc <ngominhduc100@gmail.com>
2023-08-22 16:14:01 +0700

    add AccountDTO

---
+++ src/main/java/swpdemo/openworld/dto/AccountDTO.java
@@ -0,0 +1,22 @@
+package swpdemo.openworld.dto;
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDTO {
+    private Integer id;
+    private String fullName;
+    private String username;
+    private String password;
+    private Integer status;
+    private Integer role;
+    private Date createDate;
+    private Date dob;
+    private Integer gender;
+}
