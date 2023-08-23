b3775a49b50f0b12763a7c6ebea9b1989d057a2f
daylapvlong <101397074+daylapvlong@users.noreply.github.com>
2023-08-23 19:32:10 +0700

    frontend update

--- src/main/java/swpdemo/openworld/controller/ProfileController.java
+++ src/main/java/swpdemo/openworld/controller/ProfileController.java
@@ -1,17 +1,16 @@
-package swpdemo.openworld;
+package swpdemo.openworld.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/signup")
-public class signupController {
+@RequestMapping("/profile")
+public class ProfileController {
     @GetMapping
-    public String loginView(Model model){
-        return("signup");
+    public String profilePage(Model model){
+        return("profile");
     }
 
 }
