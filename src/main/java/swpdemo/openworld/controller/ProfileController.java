14d36cad5635293e4824ab9e9c7fd832960f9144
Minh Duc <ngominhduc100@gmail.com>
2023-08-25 00:54:06 +0700

    feat: add profile module for profile page&add profileService

--- src/main/java/swpdemo/openworld/controller/ProfileController.java
+++ src/main/java/swpdemo/openworld/controller/ProfileController.java
@@ -1,15 +1,25 @@
 package swpdemo.openworld.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import swpdemo.openworld.model.Account;
+import swpdemo.openworld.model.Profile;
+import swpdemo.openworld.services.servicesimpl.ProfileService;
 
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
+    @Autowired
+    ProfileService profileService;
     @GetMapping
-    public String profilePage(Model model){
+    public String profilePage(Model model, HttpSession session){
+        Account account = (Account)session.getAttribute("account");
+        Profile profile = profileService.GetProfileByAccountId(account.getId());
+        model.addAttribute("profile", profile);
         return("profile");
     }
 
